47ea85dbdc684e6f672bf289ed1a6bf6495b07c2
USER <USER@com203-79>
2017-12-28 16:52:29 +0900

    현우 첫번째 업로드

--- SpringMyBatisNewBoard20/src/main/java/com/company/smnb20/Controller/MemberController.java
+++ SpringMyBatisNewBoard20/src/main/java/com/company/smnb20/Controller/MemberController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
